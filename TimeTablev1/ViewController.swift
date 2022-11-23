10b2e98c5599667161c2d95bd1c320f52684f3de
Isa199510 <isa.arbuhov@mail.ru>
2022-11-23 19:02:27 +0600

    Initial Commit

---
+++ TimeTablev1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TimeTablev1
+//
+//  Created by Иса on 23.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
